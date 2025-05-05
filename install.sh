d0afbd80cfb34df8cdc44dba73f59bb535b1fcd8
echoheryanto94 <echoheryanto94@gmail.com>
2025-05-05 22:25:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,14 @@ echo "[+] Updating system & installing dependencies..."
 sudo apt update
 sudo apt install -y git wget screen build-essential libcurl4-openssl-dev libjansson-dev libssl-dev automake autotools-dev
 
-echo "[+] Cloning VerusMiner repository..."
-git clone https://github.com/VerusCoin/verusminer.git
-cd verusminer
+echo "[+] Cloning VerusMiner from monkins1010..."
+git clone https://github.com/monkins1010/ccminer.git
+cd ccminer
 
 echo "[+] Building VerusMiner..."
-./autogen.sh
-./configure CXX=g++ 
-make -j$(nproc)
+./build.sh
 
-echo "[+] Downloading mining.sh from GitHack..."
+echo "[+] Downloading mining.sh..."
 wget -q -O mining.sh https://raw.githack.com/echoheryanto94/filesal/main/mining.sh
 chmod +x mining.sh
 
